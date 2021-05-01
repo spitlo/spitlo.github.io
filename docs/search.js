3f7f5d2c3b40a6d703f81d70d94b023399ab1bcf
Jakob Bergman <jakob@aino.se>
2021-05-01 07:09:26 +0200

    Deploy version 1.4.0

--- docs/search.js
+++ docs/search.js
@@ -14,7 +14,7 @@ var stemmedTerms=terms.map(function(w){return elasticlunr.stemmer(w.toLowerCase(
 var termFound=false
 var index=0
 var weighted=[]
-var sentences=body.toLowerCase().split('. ')
+var sentences=body.toLowerCase().trim().replace(/(?:\r\n|\r|\n)/g,' ').split('. ')
 for(var i in sentences){var words=sentences[i].split(' ')
 var value=FIRST_WORD_WEIGHT
 for(var j in words){var word=words[j]
@@ -41,27 +41,28 @@ maxSumIndex=i}}}
 var teaser=[]
 var startIndex=weighted[maxSumIndex][2]
 for(var i=maxSumIndex;i<maxSumIndex+windowSize;i++){var word=weighted[i]
+if(i===maxSumIndex&&word[1]!==FIRST_WORD_WEIGHT){teaser.push('…')}
 if(startIndex<word[2]){teaser.push(body.substring(startIndex,word[2]))
 startIndex=word[2]}
-if(word[1]===TERM_WEIGHT){teaser.push('<b>')}
+if(word[1]===TERM_WEIGHT){teaser.push('<strong>')}
 startIndex=word[2]+word[0].length
 teaser.push(body.substring(word[2],startIndex))
-if(word[1]===TERM_WEIGHT){teaser.push('</b>')}}
+if(word[1]===TERM_WEIGHT){teaser.push('</strong>')}}
 teaser.push('…')
 return teaser.join('')}
 function getResultSection(result){var url=result?result.ref:''
 if(url.match(/\/code\//)){return'code'}else if(url.match(/\/music\//)){return'music'}else if(url.match(/\/tags\//)){return'tags'}}
-function formatSearchResultItem(item,terms){return'<div id="search-results-item" class="'+getResultSection(item)+'">'
+function formatSearchResultItem(item,terms){return'<div class="'+getResultSection(item)+'">'
 +'<a href="'+item.ref+'">'
 +'<div class="title">'+item.doc.title+'</div>'
 +'<div class="teaser">'+makeTeaser(item.doc.body||item.doc.description,terms)+'</div>'
 +'<div class="help">ENTER</div></a></div>'}
 function initSearch(){var $searchInput=document.getElementById('search')
-var $searchResults=document.getElementById('search-results')
-var $searchResultsItems=document.getElementById('search-results-items')
-var MAX_ITEMS=10
+var $searchResults=document.getElementById('searchResults')
+var $searchResultsItems=document.getElementById('searchResultsItems')
+var MAX_ITEMS=8
 if(!$searchInput){return}
-var options={bool:'AND',fields:{title:{boost:2},body:{boost:1},},expand:true,}
+var options={bool:'OR',fields:{title:{boost:2},body:{boost:1},},expand:true}
 var currentTerm=''
 var index=elasticlunr.Index.load(window.searchIndex)
 var selectedIndex=0
@@ -70,7 +71,8 @@ function clearSearch(){selectedIndex=0
 results=[]
 $searchResultsItems.innerHTML=''
 $searchInput.value=currentTerm=''
-$searchInput.blur()}
+$searchInput.blur()
+$searchResults.style.display='none'}
 $searchInput.addEventListener('keyup',debounce(function(event){var term=$searchInput.value.trim()
 if(term===currentTerm||!index){return}
 $searchResults.style.display=term===''?'none':'block'
@@ -89,5 +91,7 @@ if(term===''){return}
 if(event.key==='ArrowUp'||event.key==='ArrowDown'){if($searchResultsItems.children&&$searchResultsItems.children[selectedIndex-1]){$searchResultsItems.children[selectedIndex-1].className=''}
 if(event.key==='ArrowUp'){if(selectedIndex>1){selectedIndex--}else{selectedIndex=results.length}}else if(event.key==='ArrowDown'){if(selectedIndex<results.length){selectedIndex++}else{selectedIndex=1}}
 if($searchResultsItems.children&&$searchResultsItems.children[selectedIndex-1]){$searchResultsItems.children[selectedIndex-1].className='selected'}}else if(event.key==='Enter'){result=results[selectedIndex-1]
-if(result){document.location.href=result.ref}}})}
+if(result){document.location.href=result.ref}}})
+$searchInput.addEventListener('search',function(event){var term=$searchInput.value.trim()
+if(!term){clearSearch()}})}
 if(document.readyState==='complete'||(document.readyState!=='loading'&&!document.documentElement.doScroll)){initSearch()}else{document.addEventListener('DOMContentLoaded',initSearch)}
