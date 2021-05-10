f202b58a8b3d56ce5512b1c411b0d039121bd811
Jakob Bergman <jakob@aino.se>
2021-05-10 07:49:29 +0200

    Deploy version 1.9.2

--- docs/games/oxygen-chase/game.js
+++ docs/games/oxygen-chase/game.js
@@ -11,7 +11,7 @@
 // oxygen, knowing her father only had two hours worth of oxygen left in his tank.
 
 const TRAVEL_TIMES = {
-  hostpital: 21,
+  hospital: 21,
   doctor: 18,
   redcross: 13,
 }
@@ -21,10 +21,10 @@ const BEDROOM_DESCS = {
 }
 
 const printGPS = () => {
-  const timeToDad = TRAVEL_TIMES[disk.leavingRoom]
+  const timeToDad = disk.leavingRoom ? TRAVEL_TIMES[disk.leavingRoom] : 0
   println('There are a few saved locations that seems interesting:')
   if (disk.leavingRoom !== 'downtown') {
-    println(`**Downtown** to the Hospital. The drive there is ${TRAVEL_TIMES.hostpital + (timeToDad / 2)} minutes.`)
+    println(`**Downtown** to the Hospital. The drive there is ${TRAVEL_TIMES.hospital + (timeToDad / 2)} minutes.`)
   }
   if (disk.leavingRoom !== 'midtown') {
     println(`**Midtown** to a doctor’s office. The drive there is ${TRAVEL_TIMES.doctor + (timeToDad / 2)} minutes.`)
@@ -37,37 +37,95 @@ const printGPS = () => {
   }
 }
 
+const enterLocation = (location) => {
+  // If we came to this location from a location other than father’s,
+  // add some extra time (don’t ask me to draw a map of this town).
+  let extraTime = 0
+  if (disk.leavingRoom && disk.leavingRoom !== location) {
+    extraTime = TRAVEL_TIMES[disk.leavingRoom] / 2
+  }
+  decreaseTimer(TRAVEL_TIMES.hospital + extraTime)
+  disk.leavingRoom = location
+  console.debug(`Entering ${location}. Timer is now ${disk.timer}`)
+  // Check if "car" room has exit "dad", otherwise add it
+  const car = getRoom('car')
+  const bedroom = car.exits.find((exit) => exit.id === 'bedroom')
+  if (!bedroom) {
+    car.exits.push(        {
+      dir: ['dad', 'dad’s', 'bedroom'],
+      id: 'bedroom',
+    })
+  }
+}
+
 const chance = (seed) => seed >= Math.random()
 
 const decreaseTimer = (subtrahend) => {
   disk.timer -= subtrahend
 }
 
-const checkOnDad = (timer) => {
-  if (chance(1/4)) {
-    println(`You only have ${timer} minutes to save your father. Perhaps you should just stay with him and talk?`)
-  } else if (chance(1/5)) {
-    const whiteLie = Math.ceil(timer / 10) * 10
-    println('"How much oxygen do we have left?", your dad asks.')
-    println(`"About ${whiteLie} minutes worth, dad. Maybe a bit more," you answer. But you know it’s less.`)
+const getName = (character) => {
+  const char = getCharacter(character, disk.character)
+  const namesLength = char.name.length
+  return namesLength === 0 ? char[0] : char[namesLength - 1]
+}
+
+const checkOnDad = () => {
+/**
+ * Covid patients appearently do not struggle with breathing, but they might
+ * breath faster (and faster?) due to lack of oxygen in the blood. This is
+ * called silent (or happy) hypoxia.
+ */
+  let breathing
+  let extra = ''
+  let breathingClass = 'breathing-normal'
+  if ((disk && disk.leak) || disk.timer < 10) {
+    breathing = 'hindered'
+    extra = ' His face is turning blue as he tries to remove the face mask.'
+    breathingClass = 'breathing-hindered'
+  } else if (disk.timer > 100) {
+    breathing = 'normal'
+  } else if (disk.timer < 101 && disk.timer > 60) {
+    breathing = 'hurried'
+    breathingClass = 'breathing-hurried'
+  } else if (disk.timer > 30) {
+    breathing = pickOne(['heavy', 'fast'])
+    extra = ' His lips and fingers are faintly blue.'
+    breathingClass = 'breathing-fast'
+  } else {
+    breathing = 'rapid and shallow'
+    extra = ' His entire skin has a blue tint. His looks slightly panicked.'
+    breathingClass = 'breathing-rapid'
+  }
+  println(`His breathing seems ${breathing}.${extra || ''}`, breathingClass)
+  if (disk.leak) {
+    println('You really, really should try to reconnect the tube to the tank.')
+  } else {
+    if (chance(1/4)) {
+      println(`You only have ${disk.timer} minutes to save your father. Perhaps you should just stay with him and talk?`)
+    } else if (chance(1/5)) {
+      const whiteLie = Math.ceil(disk.timer / 10) * 10
+      println('"How much oxygen do we have left?", your dad asks.')
+      println(`"About ${whiteLie} minutes worth, dad. Maybe a bit more," you answer. But you know it’s less.`)
+    }
   }
 }
 
-const getEmotion = (timer) => {
+const getEmotionAdverb = () => {
   /**
    * Returns an emotion based on the timer.
    * TODO: Are the different words in the correct position on the scale?
    */
   let emotion
-  if (timer > 100) {
+  if (disk.timer > 100) {
     emotion = ' '
-  } else if (timer < 101 && timer > 80) {
+  } else if (disk.timer < 101 && disk.timer > 80) {
     emotion = ` ${pickOne(['restlessly', 'nervously'])} `
-  } else if (timer > 60) {
+  } else if (disk.timer > 60) {
     emotion = ` ${pickOne(['worriedly', 'uneasily'])} `
-  } else if (timer > 40) {
+  } else if (disk.timer > 40) {
     emotion = ` ${pickOne(['timorously', 'apprehensively'])} `
-  } else if (timer > 20) {
+  } else if (disk.timer > 20) {
     emotion = ` ${pickOne(['disheartenedly', 'anxiously'])} `
   } else {
     emotion = ' despondently '
@@ -75,6 +133,10 @@ const getEmotion = (timer) => {
   return emotion
 }
 
+// ___  _ ____ _  _
+// |  \ | [__  |_/
+// |__/ | ___] | \_
+// ------------ ---  --- >
 const oxygenChase = {
   roomId: 'bedroom',
   timer: 120,
@@ -96,6 +158,7 @@ const oxygenChase = {
         if (TRAVEL_TIMES.hasOwnProperty(disk.leavingRoom)) {
           decreaseTimer(TRAVEL_TIMES[disk.leavingRoom])
           disk.leavingRoom = ''
+          checkOnDad()
         }
       },
       onLook: () => {
@@ -105,13 +168,15 @@ const oxygenChase = {
         if (disk.leak) {
           decreaseTimer(10)
           println(BEDROOM_DESCS.leak)
+          println(`You${getEmotionAdverb()}look at your dad.`)
+          checkOnDad()
         } else {
           decreaseTimer(1)
           println(BEDROOM_DESCS.original)
         }
 
-        const phone = bedroom.items.filter((item) => item.name === 'phone').length > 0
-        const keys = bedroom.items.filter((item) => item.name === 'car keys').length > 0
+        const phone = bedroom.items.find((item) => item.name === 'phone')
+        const keys = bedroom.items.find((item) => item.name === 'car keys')
         if (phone && keys) {
           println('Your **car keys** and **phone** are on the bedside table. ')
         } else if (phone) {
@@ -123,21 +188,22 @@ const oxygenChase = {
         if (disk.leak) {
           println('There is a **loose tube** here, and the oxygen tank is leaking.')
         } else {
-          checkOnDad(disk.timer)
+          checkOnDad()
         }
       },
       items: [
         {
           name: ['oxygen tank', 'gauge'],
           desc: 'It’s a big oxygen tank, strapped to a cart.',
-          onLook: () => println(`You${getEmotion(disk.timer)}check the gauge on the tank. There seems to be about ${disk.timer} minutes of oxygen left.`),
+          onLook: () => println(`You${getEmotionAdverb()}check the gauge on the tank. There seems to be about ${disk.timer} minutes of oxygen left.`),
         },
         {
           name: 'phone',
-          desc: 'Perhaps there is someone you can call?',
+          desc: 'Who could you call?',
           isTakeable: true,
           onUse: () => {
-            console.log('Using phone')
+            println('You scroll through all your contacts, but you can’t think of anyone who could help you.')
+            decreaseTimer(5)
           },
         },
         {
@@ -149,7 +215,7 @@ const oxygenChase = {
             if (chance(1/2)) {
               decreaseTimer(10)
               disk.leak = true
-              println('The tubing pulls hard on the tank, and suddenly snaps loose. At first, you think you broke it, but it is only disconnected from the tank. You scramble frantically to reconnect it as the precious oxygen leaks out into the room.')
+              println('It pulls hard on the tank, and suddenly snaps loose. At first, you think you broke it, but it is only disconnected from the tank. Precious oxygen leaks out into the room.')
               const bedroom = getRoom('bedroom')
               bedroom.items.push({
                 name: ['loose tube', 'tubing'],
@@ -157,7 +223,7 @@ const oxygenChase = {
                 isTakeable: true,
                 onUse: () => {
                   decreaseTimer(10)
-                  println('You struggle with it, but after a few minutes you are able to reconnect the tube to the tank.')
+                  println('You scramble frantically to reconnect the tube to the tank, and after a few minutes the leaking stops.')
                   println(`Examining the gauge, you can see there is about ${disk.timer} minutes of oxygen left in the tank.`)
                   disk.hasLeaked = true
                   disk.leak = false
@@ -195,6 +261,7 @@ const oxygenChase = {
       },
       onLook: () => {
         if (disk.gps) {
+          println('You check the GPS.')
           printGPS()
         }
       },
@@ -205,7 +272,9 @@ const oxygenChase = {
             if (disk.gps) {
               printGPS()
             } else {
-              println('The GPS boots up. The animation seemingly repeats forever, but eventually you’re presented with the welcome screen. Where do you want to go?')
+              println('The GPS boots up. The animation seemingly repeats forever, but eventually you’re presented with the welcome screen.')
+              printGPS()
+              println('Where do you want to go?')
               disk.gps = true
               decreaseTimer(2)
               const car = getRoom('car')
@@ -241,19 +310,15 @@ const oxygenChase = {
     {
       id: 'hospital',
       name: 'At the hospital',
-      desc: '',
+      desc: `You’re standing in the lobby of the town’s hospital. Plastic plants adorn the corners of the room, and dashed yellow lines on the floor disappear off behind closed doors.
+
+A **receptionist** sits behind a huge desk.`,
       exits: [{
         dir: ['car', 'outside'],
         id: 'car',
       }],
       onEnter: () => {
-        let extraTime = 0
-        if (disk.leavingRoom) {
-          // We came from somewhere other than father’s, add some extra time
-          extraTime = TRAVEL_TIMES[disk.leavingRoom] / 2
-        }
-        decreaseTimer(TRAVEL_TIMES.hospital + extraTime)
-        disk.leavingRoom = 'hospital'
+        enterLocation('hospital')
       },
     },
     {
@@ -265,12 +330,7 @@ const oxygenChase = {
         id: 'car',
       }],
       onEnter: () => {
-        let extraTime = 0
-        if (disk.leavingRoom) {
-          extraTime = TRAVEL_TIMES[disk.leavingRoom] / 2
-        }
-        decreaseTimer(TRAVEL_TIMES.doctor + extraTime)
-        disk.leavingRoom = 'doctor'
+        enterLocation('doctor')
       },
     },
     {
@@ -282,12 +342,7 @@ const oxygenChase = {
         id: 'car',
       }],
       onEnter: () => {
-        let extraTime = 0
-        if (disk.leavingRoom) {
-          extraTime = TRAVEL_TIMES[disk.leavingRoom] / 2
-        }
-        decreaseTimer(TRAVEL_TIMES.redcross + extraTime)
-        disk.leavingRoom = 'redcross'
+        enterLocation('redcross')
       },
     },
   ],
@@ -297,31 +352,11 @@ const oxygenChase = {
       roomId: 'bedroom',
       desc: 'You look at your father with affection. He smiles back at you.',
       onLook: () => {
-        /**
-         * Covid patients appearently do not struggle with breathing, but they might
-         * breath faster (and faster?) due to lack of oxygen in the blood. This is
-         * called silent (or happy) hypoxia.
-         */
-        let breathing
-        let extra = ''
-        console.log('Timer', disk.timer)
-        if (disk.timer > 100) {
-          breathing = 'normal'
-        } else if (disk.timer < 101 && disk.timer > 60) {
-          breathing = 'hurried'
-        } else if (disk.timer > 30) {
-          breathing = pickOne(['heavy', 'fast'])
-          extra = ' His lips and fingers are faintly blue.'
-        } else {
-          breathing = 'rapid and shallow'
-          extra = ' His entire skin has a blue tint. His looks slightly panicked.'
-        }
-        println(`His breathing seems ${breathing}.${extra || ''}`)
-        checkOnDad(disk.timer)
+        checkOnDad()
       },
       topics: [
         {
-          option: '**Covid**',
+          option: '**Covid**, huh?',
           line: '',
           keyword: 'covid',
           removeOnRead: true,
@@ -330,12 +365,107 @@ const oxygenChase = {
           },
         },
         {
-          option: '**Mother**',
+          option: 'Look, dad. I spoke to **Mother**.',
           line: '',
           keyword: 'mother',
           removeOnRead: true,
           onSelected: () => {
             decreaseTimer(10)
+          },
+        },
+        {
+          option: '**How** are you, dad?',
+          line: '',
+          keyword: 'how',
+          removeOnRead: false,
+          onSelected: () => {
+            decreaseTimer(1)
+            checkOnDad()
+          },
+        },
+      ],
+    },
+    {
+      name: ['receptionist'],
+      roomId: 'hospital',
+      desc: 'A **receptionist** sits behind a huge desk, reading something on his phone. A name tag on his chest reads "René".',
+      onLook: () => {
+        getCharacter('receptionist').name.push('René')
+      },
+      topics: [
+        {
+          option: 'Ask for **oxygen**',
+          line: `"I need an oxygen tank," you say. "I’ll pay for it, if that’s what you need. No questions asked."
+
+The receptionist looks up at you. "Does this look like a dive shop?" he spits.`,
+          keyword: 'oxygen',
+          removeOnRead: true,
+          onSelected: () => decreaseTimer(1),
+        },
+        {
+          option: 'Ask to speak to an **administrator**',
+          line: `"I want to speak to an administrator," you say sternly.
+
+René shuffles a few papers, restarts a lucky cat whose arm is slowing down somewhat, and picks up his phone again.
+
+"The administrator is busy," he mumbles.`,
+          keyword: 'administrator',
+          prereqs: ['oxygen'],
+          removeOnRead: true,
+          onSelected: () => decreaseTimer(2),
+        },
+        {
+          option: '**Insist** on speaking to an administrator',
+          line: `"Put down your phone and look at me," you half-scream at the receptionist.
+
+"I insist, I really need to speak to an administrator."`,
+          keyword: 'insist',
+          prereqs: ['administrator'],
+          removeOnRead: true,
+          onSelected: () => decreaseTimer(3),
+        },
+        {
+          option: '**Beg** to speak to an administrator',
+          line: `"Listen. I really need to see an administrator. I need oxygen, badly."
+"My father barely has any left in his tank."
+
+"He is dying."`,
+          keyword: 'beg',
+          prereqs: ['insist'],
+          removeOnRead: true,
+          onSelected: () => decreaseTimer(4),
+        },
+        {
+          option: '**Demand** to speak to a god damned administrator',
+          line: `You bang your fist in the desk. "Call up a god damned administrator," you scream. "I’m not asking!"
+
+The receptionist looks up again, unamused. Slowly, he slides the desk phone closer and begins to dial a number, laboriously pressing each key and pausing. "There," he says after an eternity. Then, into the mouthpiece, "This is René in the reception. Can you come down? Uh-mmm, yeah." He looks back at you and continues, "Your wish is my command."`,
+          keyword: 'demand',
+          prereqs: ['beg'],
+          removeOnRead: true,
+          onSelected: () => {
+            decreaseTimer(8)
+            disk.characters.push(
+              {
+                name: ['administrator', 'hospital administrator'],
+                roomId: 'hospital',
+                desc: 'The hospital **administrator**, a woman in her late fifties or early sixties with ash-blond hair and an uninviting smile.',
+                onLook: () => {
+                  getCharacter('administrator').name.push('Catrine')
+                },
+                topics: [
+                  {
+                    option: 'Ask for **oxygen**',
+                    line: '"As I told, um, René here," you say, glancing at the receptionist’s name tag, "I’m in dire need of oxygen. My father has precious little left in his tank. Please, can you help me?"',
+                    keyword: 'oxygen',
+                    onSelected: () => decreaseTimer(4),
+                  },
+                ],
+              },
+            )
+            println(`After a few minutes, an elevator dings as the doors slide up.
+[...]
+"I’m the administrator," she says. "And you are?"`)
           },
         },
       ],
