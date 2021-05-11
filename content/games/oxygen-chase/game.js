f5c28a9b31965690995151afed80ae366aa2f731
Jakob Bergman <jakob@aino.se>
2021-05-12 00:09:21 +0200

    Oxygen Chase: Start in bedroom

--- content/games/oxygen-chase/game.js
+++ content/games/oxygen-chase/game.js
@@ -138,7 +138,7 @@ const getEmotionAdverb = () => {
 // |__/ | ___] | \_
 // ------------ ---  --- >
 const oxygenChase = {
-  roomId: 'hospital',
+  roomId: 'bedroom',
   timer: 120,
   gps: false,
   leak: false,
