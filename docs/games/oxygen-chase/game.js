ac2681368a30a140d72982e58b5efbe3b56ad993
Jakob Bergman <jakob@aino.se>
2021-05-19 23:38:01 +0200

    Deploy version 1.10.4

--- docs/games/oxygen-chase/game.js
+++ docs/games/oxygen-chase/game.js
@@ -12,7 +12,6 @@ const enterLocation=(location)=>{let extraTime=0
 if(disk.leavingRoom&&disk.leavingRoom!==location){extraTime=TRAVEL_TIMES[disk.leavingRoom]/2}
 decreaseTimer(TRAVEL_TIMES.hospital+extraTime)
 disk.leavingRoom=location
-console.debug(`Entering ${location}. Timer is now ${disk.timer}`)
 const car=getRoom('car')
 const bedroom=car.exits.find((exit)=>exit.id==='bedroom')
 if(!bedroom){car.exits.push({dir:['dad','dad’s','bedroom'],id:'bedroom',})}}
@@ -32,9 +31,9 @@ const carExit=getExit('car',bedroom.exits)
 carExit.block='The oxygen tank is leaking and your father is dying from hypoxia. Is now really a good time to leave?'}
 const chance=(value)=>value>=Math.random()
 const decreaseTimer=(subtrahend)=>{disk.timer-=subtrahend}
-const getName=(character)=>{const char=getCharacter(character,disk.character)
+const getName=(character,capitalize=true)=>{const char=getCharacter(character)
 const namesLength=char.name.length
-return namesLength===0?char[0]:char[namesLength-1]}
+return namesLength===1?`${capitalize ? 'T' : 't'}he ${character}`:char.name[namesLength-1]}
 const checkOnDad=()=>{let breathing
 let extra=''
 let breathingClass='breathing-normal'
@@ -53,7 +52,7 @@ println(`"About ${whiteLie} minutes worth, dad. Maybe a bit more," you answer. B
 const getEmotionAdverb=()=>{let emotion
 if(disk.timer>100){emotion=' '}else if(disk.timer<101&&disk.timer>80){emotion=` ${pickOne(['restlessly', 'nervously'])} `}else if(disk.timer>60){emotion=` ${pickOne(['worriedly', 'uneasily'])} `}else if(disk.timer>40){emotion=` ${pickOne(['timorously', 'apprehensively'])} `}else if(disk.timer>20){emotion=` ${pickOne(['disheartenedly', 'anxiously'])} `}else{emotion=' despondently '}
 return emotion}
-const oxygenChase={roomId:'bedroom',timer:120,gps:false,leak:false,hasLeaked:false,leavingRoom:'',inventory:[],rooms:[{id:'bedroom',img:`
+const oxygenChase={roomId:'hospital',timer:120,gps:false,leak:false,hasLeaked:false,leavingRoom:'',inventory:[],rooms:[{id:'bedroom',img:`
    ░░ ░  ░ ░ ░  ░░  ░░░ ░  ░
   ░  ░ ░░  ░░░ ░    ░   ░░ ░
   ▒  ▒ ▒▒  ▒▒▒ ▒ ▒▒ ▒▒  ▒ ▒▒
@@ -101,26 +100,26 @@ const redcross=getExit('uptown',car.exits)
 delete redcross.block}},},],exits:[{dir:['dad','dad’s','bedroom'],id:'bedroom',},{dir:['downtown','hospital'],id:'hospital',block:'You won’t be able to find your way to the hospital without the **GPS**.',},{dir:['midtown','doctor'],id:'doctor',block:'There’s no way you’ll get to the doctor’s office without the **GPS**.',},{dir:['uptown','redcross'],id:'redcross',block:'You haven’t got a clue where the Red Cross is located. Perhaps you should use the **GPS**?',},],},{id:'hospital',name:'At the hospital',desc:`You’re standing in the lobby of the town’s hospital. Plastic plants adorn the corners of an otherwise drab room. Dashed yellow lines on the floor taper off and disappear, mostly, behind closed doors.
 
 A **receptionist** sits behind a huge, beige desk.`,exits:[{dir:['car','outside'],id:'car',}],onEnter:()=>{enterLocation('hospital')},},{id:'doctor',name:'At the doctor’s office',desc:'',exits:[{dir:['car','outside'],id:'car',}],onEnter:()=>{enterLocation('doctor')},},{id:'redcross',name:'At the Red Cross',desc:'',exits:[{dir:['car','outside'],id:'car',}],onEnter:()=>{enterLocation('redcross')},},],characters:[{name:['father','dad'],roomId:'bedroom',desc:'You look at your father with affection. He smiles back at you.',onLook:()=>{checkOnDad()},topics:[{option:'That **covid**, huh?',line:'',keyword:'covid',removeOnRead:true,onSelected:()=>{decreaseTimer(5)},},{option:'Look, dad. I spoke to **mother**.',line:'',keyword:'mother',removeOnRead:true,onSelected:()=>{decreaseTimer(10)},},{option:'**How** are you feeling, dad?',line:'',keyword:'how',removeOnRead:false,onSelected:()=>{decreaseTimer(1)
-checkOnDad()},},],},{name:['receptionist'],roomId:'hospital',desc:'The receptionist appears to be in his twenties, but looks almost boyish behind the oversized desk. He’s staring at his phone. A name tag on his chest reads "René".',onLook:()=>{getCharacter('receptionist').name.push('René')},topics:[{option:'Ask for **oxygen**',line:`"Hi. I need an oxygen tank," you say. "I’ll pay for it, if that’s what you need. No questions asked."
+checkOnDad()},},],},{name:['receptionist'],roomId:'hospital',desc:'The receptionist appears to be in his twenties, but looks almost boyish behind the oversized desk. He’s staring at his phone. A name tag on his chest reads "**André**".',onLook:()=>{getCharacter('receptionist').name.push('André')},topics:[{option:'Ask for **oxygen**',line:()=>`"Hi. I need an oxygen tank," you say. "I’ll pay for it, if that’s what you need. No questions asked."
 
-The receptionist looks up at you. "Does this look like a dive shop?" he snarks.
+${getName('receptionist')} looks up at you. "Does this look like a dive shop?" he snarks.
 
-You’re not off to a good start. It’s apparent you’ll need to try a different approach.`,keyword:'oxygen',removeOnRead:true,onSelected:()=>decreaseTimer(1),},{option:'**Ask** to speak to an administrator',line:`"I want to speak to an administrator," you say sternly.
+You’re not off to a good start. It’s apparent you’ll need to try a different approach.`,keyword:'oxygen',removeOnRead:true,onSelected:()=>decreaseTimer(1),},{option:'**Ask** to speak to an administrator',line:()=>`"I want to speak to an administrator," you say sternly.
 
-René shuffles a few papers, restarts a lucky cat whose arm is slowing down somewhat, and picks up his phone again.
+${getName('receptionist')} shuffles a few papers, restarts a lucky cat whose arm is slowing down somewhat, and picks up his phone again.
 
 "The administrator is busy," he mumbles.`,keyword:'ask',prereqs:['oxygen'],removeOnRead:true,onSelected:()=>decreaseTimer(2),},{option:'**Insist** on speaking to an administrator',line:`"Put down your phone and look at me," you say in your most decisive tone.
 
 "I insist, I really need to speak to an administrator."`,keyword:'insist',prereqs:['oxygen'],removeOnRead:true,onSelected:()=>decreaseTimer(3),},{option:'**Beg** to speak to an administrator',line:`"Listen. I really need to see an administrator. I need oxygen, badly."
 "My father barely has any left in his tank."
 
-Then you add, "He is dying."`,keyword:'beg',prereqs:['oxygen'],removeOnRead:true,onSelected:()=>decreaseTimer(4),},{option:'**Demand** to speak to a god damned administrator',line:`You bang your fist on the desk. "Call up a god damned administrator," you cry out. "I’m not asking!"
+Then you add, "He is dying."`,keyword:'beg',prereqs:['oxygen'],removeOnRead:true,onSelected:()=>decreaseTimer(4),},{option:'**Demand** to speak to a god damned administrator',line:()=>`You bang your fist on the desk. "Call up a god damned administrator," you cry out. "I’m not asking!"
 
-The receptionist looks up again, unamused. Slowly, he slides the desk phone closer and begins to dial a number, laboriously moving his finger from key to key and pausing between digits. "There," he says after an eternity. Then, into the mouthpiece, "This is René in the reception. Can you come down? Uh-mmm, yeah." He looks back at you and continues, "Your wish is my command."`,keyword:'demand',prereqs:['ask','insist','beg'],removeOnRead:true,onSelected:()=>{decreaseTimer(8)
+${getName('receptionist')} looks up again, unamused. Slowly, he slides the desk phone closer and begins to dial a number, laboriously moving his finger from key to key and pausing between digits. "There," he says after an eternity. Then, into the mouthpiece, "This is André in the reception. Can you come down? Uh-mmm, yeah." He looks back at you and continues, "Your wish is my command."`,keyword:'demand',prereqs:['ask','insist','beg'],removeOnRead:true,onSelected:()=>{decreaseTimer(8)
 println(`After a few minutes, an elevator dings as the doors slide up.
 A woman in her late fifties or early sixties glides out of the elevator and walks up to you. She leans against the desk.
 "I’m the administrator," she says. "And you are?"`)
-disk.characters.push({name:['administrator','hospital administrator'],roomId:'hospital',desc:'The hospital administrator is a woman ash-blond hair and an uninviting smile. She, too, has a name tag. It reads "Catrine".',onLook:()=>{getCharacter('administrator').name.push('Catrine')},topics:[{option:'**Introduce** yourself',line:'',keyword:'introduce',onSelected:()=>decreaseTimer(2),},{option:'Ask for **oxygen**',line:`"As I told, um, René here," you say, glancing at the receptionist’s name tag, "I’m in dire need of oxygen. My father has precious little left in his tank.
+disk.characters.push({name:['administrator','hospital administrator'],roomId:'hospital',desc:'The hospital administrator is a woman ash-blond hair and an uninviting smile. She, too, has a name tag. It reads "**Catrine**".',onLook:()=>{getCharacter('administrator').name.push('Catrine')},topics:[{option:'**Introduce** yourself',line:'',keyword:'introduce',onSelected:()=>decreaseTimer(2),},{option:'Ask for **oxygen**',line:()=>`"As I told, um, André here," you say, glancing at the receptionist’s name tag, "I’m in dire need of oxygen. My father has precious little left in his tank.
 Please, can you help me?"
 
-The administrator looks at you sullenly.`,keyword:'oxygen',onSelected:()=>decreaseTimer(4),},],},)},},],},],};export default oxygenChase
+${getName('administrator')} looks at you sullenly.`,keyword:'oxygen',onSelected:()=>decreaseTimer(4),},],},)},},],},],};export default oxygenChase
