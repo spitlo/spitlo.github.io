c5394f071c996a341f0f8918a46c5b982cc2411d
Jakob Bergman <jakob@aino.se>
2021-05-23 16:13:02 +0200

    Deploy version 1.10.7

--- docs/lennys.mjs
+++ docs/lennys.mjs
@@ -16,7 +16,7 @@ const playGame=(gameSlug)=>{const decrunch=document.getElementById('decrunch')
 decrunch.className='visible'
 setTimeout(()=>{document.location.href=`/games/${gameSlug}/`},800)}
 const getShortName=(name)=>(name||'').split(' ')[0].toLowerCase()
-const getActionFigureSentence=()=>{const sentences=[`"This 'Gorgobot' figure from 1972 is pretty neat, it has telescopic arms and an inflatable chest piece!"`,`"I have this Kaboozle Toys 'Discombobulon' figure from the desert death match scenes that was never actually released."`,`"Yeah, the 'Konglomatron 2000' is pretty priceless, but then again, I’d never sell it so who cares, right?"`,`"All of these babies are pretty much priceless, but my personal favorite is this 'Rash Man' figure, it’s a beaut!"`,`"Yeah," he starts, then looses the thread and trails off into an adjacent room.`,`"If I had to pick one, I’d say this 'The French Tickler' figure is, ahem, 'le pièce de résistance' of my collection, as it were."`,]
+const getActionFigureSentence=()=>{const sentences=[`"This 'Gorgobot' figure from 1972 is pretty neat, it has telescopic arms and an inflatable chest piece!"`,`"I have this Kaboozle Toys’ 'Discombobulon' figure from the desert death match scenes that was never actually released."`,`"Yeah, the 'Konglomatron 2000' is pretty priceless, but then again, I’d never sell it so who cares, right?"`,`"All of these babies are pretty much priceless, but my personal favorite is this 'Rash Man' figure, it’s a beaut!"`,`"Yeah," he starts, then looses the thread and trails off into an adjacent room. Moments later he’s back.`,`"If I had to pick one, I’d say this 'The French Tickler' figure is, ahem, 'le pièce de résistance' of my collection, as it were."`,`I’d say this 'Hoo-Boy' figure is pretty priceless. The box is mint, the detachable cape is intact and it has the original blue trunks. Plus, he’s a talker!`,`I have this full set of 'She-Horse' figures, the four original mane colors from 1969 and the chestnut one released on 1970.`,]
 return pickOne(sentences)}
 const play=(gameId)=>{let game=getItemInRoom(gameId,disk.roomId)||getItemInInventory(gameId)
 if(!game){println(`You don’t seem to have that game handy.`)}else{game.onPlay()
@@ -38,11 +38,11 @@ const lennys={roomId:'lennys',rooms:[{name:'Lenny’s E-game Emporium',id:'lenny
 
 To the **west** is the Ascii Arena, where Lenny keeps all his text-based games. To the **east** is the Pixel Paradise where graphical games are displayed. To the right of the counter, there’s a big **crate**. The sign on it says "Discounted games".
 
-**Lenny** is here, carefully dusting a collection of action figures housed in a display **cabinet** to the left of the counter.`,exits:[{dir:['west','Ascii Arena','Text Games'],id:'text-games'},{dir:['east','Pixel Paradise','Graphical Games'],id:'graphical-games'},],items:[{name:['crate','discount crate','discounted games'],desc:'Ths discount crate is empty at the moment.'},{name:['cabinet','display cabinet','action figures'],desc:`You lean over and inspect the display cabinet. There are some real gems in there.
+**Lenny** is here, carefully dusting a collection of action figures housed in a display **cabinet** to the left of the counter.`,exits:[{dir:['west','Ascii Arena','Text Games'],id:'text-games'},{dir:['east','Pixel Paradise','Graphical Games'],id:'graphical-games'},{dir:['south','out','exit'],id:'exit'},],items:[{name:['crate','discount crate','discounted games'],desc:'Ths discount crate is empty at the moment.'},{name:['cabinet','display cabinet','action figures'],desc:`You lean over and inspect the display cabinet. There are some real gems in there.
 "No touchy-touchy," Lenny says. "Only looky-looky."`,},],},{name:'The Ascii Arena',id:'text-games',desc:`There is a huge banner covering the entire west wall of the room. It reads:
 "This is the Dungeon dungeon, the MUD swamp, the Infocom inferno, this is the domain of King Text, the seven bit wonder of the world, the web of the eight bit spider. This is Room 437. This is the Ascii Arena."
 
-Theres a **note** pinned to the banner. The **shelves** are remarkably empty. An opening in the **east** wall leads back to the front room.`,onLook:()=>{println('You’re standing in the Ascii Arena, as Lenny somewhat hyperbolically has christened this tiny room holding all his text-based adventure games.')},items:[{name:['shelves','games'],desc:`There are a few games lining the shelves, but most of them have giant stickers saying "PRIVATE COLLECTION! NOT FOR SALE, RENT OR USE." The only available games are:
+Theres a **note** pinned to the banner. The **shelves** are remarkably empty. An opening in the **east** wall leads back to the front room.`,exits:[{dir:['east'],id:'lennys'}],onLook:()=>{println('You’re standing in the Ascii Arena, as Lenny somewhat hyperbolically has christened this tiny room holding all his text-based adventure games.')},items:[{name:['shelves','games'],desc:`There are a few games lining the shelves, but most of them have giant stickers saying "PRIVATE COLLECTION! NOT FOR SALE, RENT OR USE." The only available games are:
 ${window.games.map((game) => `-${game.title}(**${getShortName(game.title)}**)`)}`,onLook:()=>{const room=getRoom('text-games')
 let newItems=window.games.filter((game)=>game.extra&&game.extra.room==='text-games').map((game)=>{const shortName=getShortName(game.title)
 return{name:[game.title],desc:`The back of the box reads:
@@ -57,8 +57,25 @@ DISCLAIMER!
 These games are actually encoded in UTF-8, not ASCII. Sue me.
 
 // LENNY`,onTake:()=>println('Let’s assume Lenny put it there for a reason.')},],},{name:'The Pixel Paradise',id:'graphical-games',desc:`This room is empty at the moment. Perhaps Lenny is pivoting toward text adventures?
-A door to the **west** leads back to the main room.`,exits:[{dir:'west',id:'lennys'},],items:window.games.filter((game)=>game.extra&&game.extra.room==='graphical-games').map((game)=>({name:[game.title]}))},],characters:[{name:['Lenny'],roomId:'lennys',desc:'Lenny doesn’t look anything at all like you’d picture a purveyor of retro games and action figures. In fact, judging by his jawline alone, you’d have him pegged as a personal trainer for the elderly or a retired police officer. But his tight, sleeveless t-shirt says "I got the Babel Fish", and on his left wrist is a Casio C-80 calculator watch.',topics:[{option:'Compliment Lenny’s display **cabinet**',keyword:'cabinet',removeOnRead:false,line:()=>`"Wow, Lenny. That’s some hero collection you’ve got there," you say. Lenny looks up, self-assured. You continue. "Any priceless ones?"
-"Well..." Lenny scratches his stubble. ${getActionFigureSentence()}`,},...window.games.map((game)=>{const shortName=getShortName(game.title)
+A door to the **west** leads back to the main room.`,exits:[{dir:'west',id:'lennys'},],items:window.games.filter((game)=>game.extra&&game.extra.room==='graphical-games').map((game)=>({name:[game.title]}))},{name:'Leaving Lenny’s',id:'exit',onEnter:()=>{println(`"Bye, Lenny," you say. "I’ll see you another day."
+Lenny looks up at you, surprised that you are still there. He gives you a vacant thumbs-up, then continues dusting.`)
+setTimeout(()=>{document.body.className='exiting'
+const frameElement=window.frameElement
+if(frameElement){setTimeout(()=>{frameElement.className='closed'
+setTimeout(()=>{frameElement.parentElement.remove()},1200)},1500)}else{setTimeout(()=>{window.location.href='/'},2000)}},4000)}},],characters:[{name:['Lenny'],roomId:'lennys',desc:`Lenny doesn’t look anything at all like you’d picture a purveyor of retro games and action figures. In fact, judging by his jawline alone, you’d have him pegged as a personal trainer for the elderly or a retired police officer. But his tight, sleeveless t-shirt says "I got the Babel Fish", and on his left wrist is a Casio C-80 calculator watch.
+He’s busy dusting his collection of action figures.`,onTalk:()=>{println(`You clear your throat loudly. Nothing happens. "Lenny," you say. Lenny starts whistling.
+"Oh, mighty keeper of... the realm?" you submit. Lenny stops dusting momentarily and glances at you.
+"I, your humble servant, eh, haveth... an inquisition?" You give up. "Hey, Lenny, stop dusting. I want to ask you something."
+Lenny puts his feather duster in his belt fancily -- like a modern-day, domiciliary Zorro -- and steps over to you.`)
+const lenny=getCharacter('lenny')
+delete lenny.onTalk},topics:[{option:'Compliment Lenny’s display **cabinet**',keyword:'cabinet',removeOnRead:false,line:()=>`${
+            getCharacter('lenny').chatLog.includes('cabinet')
+              ? '"Awesome collection! Any other favorites?"'
+              : '"Wow, Lenny. That’s some hero collection you’ve got there," you say. Lenny looks up, self-assured. You continue, "Any priceless ones?"'
+            }
+"Well..." Lenny scratches his stubble. ${
+  getActionFigureSentence()
+}`,onSelect:()=>{const hasTalked=getCharacter('lenny').chatLog.includes('cabinet');},},...window.games.map((game)=>{const shortName=getShortName(game.title)
 return{option:`Ask Lenny about ${game.title} (**${shortName}**)`,keyword:shortName,removeOnRead:false,line:`"Hey, Lenny," you smile. "I’m looking for a title, '${game.title}'. Do you know it?"
 Lenny sighs, than begrudingly limps away to the next room. When he returns, ha has a game box in his hand. He lets his reading glasses slide from his forehead down the length of his nose. Then, keeping the box at arms length, he reads from the back:
 "${(game.description || '').toUpperCase()}"
